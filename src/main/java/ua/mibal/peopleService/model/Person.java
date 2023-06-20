3f3d6b302292e32f704aca0718295efe6c40678c
mibal_ua <9mohapx9@gmail.com>
2023-06-20 15:48:07 +0200

    Implement `Person` model

--- src/main/java/ua/mibal/peopleService/model/Person.java
+++ src/main/java/ua/mibal/peopleService/model/Person.java
@@ -1,6 +1,6 @@
 package ua.mibal.peopleService.model;
 
-import java.util.List;
+import java.util.Set;
 
 /**
  * @author Mykhailo Balakhon
@@ -8,10 +8,48 @@ import java.util.List;
  */
 public class Person {
 
-    // TODO
+    private int id;
+
+    private int braceletId;
+
+    private String name;
+
+    private Set<Integer> days;
+
+    private Person() {
+    }
+
+    public Person(int id, int braceletId, String name, Set<Integer> days) {
+        this.id = id;
+        this.braceletId = braceletId;
+        this.name = name;
+        this.days = days;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getBraceletId() {
+        return braceletId;
+    }
+
+    public String getName() {
+        return name;
+    }
 
     public boolean isRegisteredForDay(int dayId) {
-        // FIXME test values
-        return List.of(1, 3, 5).contains(dayId);
+        return days.contains(dayId);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Person{");
+        sb.append("id=").append(id);
+        sb.append(", braceletId=").append(braceletId);
+        sb.append(", name='").append(name).append('\'');
+        sb.append(", days=").append(days);
+        sb.append('}');
+        return sb.toString();
     }
 }
